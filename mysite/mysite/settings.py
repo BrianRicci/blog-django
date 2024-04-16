e7e1d0336d42129b794d088c0a3cc4f633fbf301
BrianRicci <yunus.ibragimov49@gmail.com>
2024-04-16 19:21:54 +0300

    Реализация отправки писем

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yunus.ibragimov49@gmail.com'
+EMAIL_HOST_PASSWORD = 'gucw vyzl nydf embf'
+EMAIL_USE_TLS = True
